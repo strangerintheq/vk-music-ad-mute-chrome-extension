04591c5acc5183f0cc51ac7d4e741726a2846d9d
strangerintheq <tripmannn@gmail.com>
2018-06-24 13:53:01 +0300

    git ignore and log msn

--- mute.js
+++ mute.js
@@ -17,6 +17,7 @@
             return;
         clearInterval(initTaskId);
         setInterval(checkAd, 100);
+        console.log('vk-audio-ad-mute extension activated');
     }
 
     function checkAd() {
